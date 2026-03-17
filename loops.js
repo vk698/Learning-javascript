e5973d60196b0684200da28b68501abdefafcfc3
vk698 <vishalkumar2257r@gmail.com>
2026-03-17 17:19:31 +0530

    learning loops

---
+++ loops.js
@@ -0,0 +1,29 @@
+// sum of 1to10
+// let a;
+// let b=0;
+// for(a=0;a<=10;a++)
+// {
+//   b=b+a;
+    
+// }
+//  console.log(b)
+
+
+// table of 2
+// let a;
+// for(let a=1;a<=10;a++)
+// {
+     
+//     console.log(`2 x ${a} =`,a*2)
+// }
+// console.log(a);
+//print even and odd numbers 1to 100.
+// for(let a=1;a<=100;a++)
+//     {
+// if(a%2==0){
+//     console.log("even",a)
+// }
+// else{
+//     console.log("odd",a)
+// }
+// }
