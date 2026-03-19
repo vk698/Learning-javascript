cbffe886ee43694a5da20a6541a26d4b62f472cd
vk698 <vishalkumar2257r@gmail.com>
2026-03-19 17:14:58 +0530

    new changes

--- loops.js
+++ loops.js
@@ -27,3 +27,26 @@
 //     console.log("odd",a)
 // }
 // }
+
+
+
+
+
+// let i=0;
+// do{
+//     console.log(i)
+//     i++;
+// }while(i<11)
+
+// let i=1;
+// do{
+//     console.log(`2x${i}=`,2*i)
+//     i++
+// }while(i<11)
+
+let a=0;
+while(a<=10)
+{
+    console.log(a)
+    a++;
+}
