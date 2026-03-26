bdfd63b93e6f0ba835a4387d1367c01a2ccfc1c7
vk698 <vishalkumar2257r@gmail.com>
2026-03-26 16:18:28 +0530

    "01"

--- function.js
+++ function.js
@@ -1,24 +1,201 @@
-let sum=(a,b) => a+b;
-console.log(sum(20,34))
+// let sum=(a,b) => a+b;
+// console.log(sum(20,34))
 
 
 
-let mul=(p,q) => 
-{
-    console.log( p*q)
-    return
-}
-mul(22,2);
+// let mul=(p,q) => 
+// {
+//     console.log( p*q)
+//     return
+// }
+// mul(22,2);
 
 
 
-let s=(...args)=>
+// let s=(...args)=>
     
-{
-    let sum=0;
-    for(let a of args) sum += a;
-    console.log(sum)
-    return
+// {
+//     let sum=0;
+//     for(let a of args) sum += a;
+//     console.log(sum)
+//     return
 
-}
-s(23,34,3,2,2,2,2,2);
+// }
+// s(23,34,3,2,2,2,2,2);
+
+
+// let a=["sumit","ronit","vishal","ujjwal"];
+// a.forEach( data =>  console.log(data.toUpperCase()));
+//  let b=a.map(data => data.toUpperCase());
+// console.log(b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Basic Function Questions :-
+
+// 1. Simple arrow Function
+//    Write a function that returns "Hello Student".
+
+// 2. Addition Function
+//    Create a function that takes two numbers and returns their sum.
+
+// 3. Even or Odd
+//    Write a function that checks if a number is even or odd.
+
+// 4. Greeting Function
+//    Create a function that takes a name and returns: Hello, <name>
+
+// 5. Default Parameter
+//    Write a function where:
+
+//    If no argument is passed, it returns "Guest"
+//    Otherwise, it returns the given name
+
+// 6. Maximum Number
+//    Write a function that takes 3 numbers and returns the largest.
+
+// 7. Sum of All Arguments
+//    Use rest parameter to create a function that adds any number of values.
+
+// 8. Arrow Function Practice
+//    Convert this into arrow function:
+
+// function multiply(a, b) {
+//   return a * b;
+// }
+ 
+// -------------------------------------------
+
+// Array + map() Questions :-
+
+// 9. Double the Numbers
+
+// Given: [1, 2, 3, 4]
+
+// Return: [2, 4, 6, 8]
+
+// 10. Convert to Squares
+
+// Return square of each number using map().
+
+// 11. Extract Names
+
+// Given: [{name: "John"}, {name: "Jane"}]
+
+// Return: ["John", "Jane"]
+
+
+
+
+
+// let a=(greeting)=> console.log("hello world");
+// a();
+
+// let sum=(a,b)=>console.log(a+b);
+// sum(23,23);
+
+// let a=(n)=>
+// {
+//     if(n%2==0)
+//     {
+//         console.log("it is a even number")
+//     }
+//     else 
+//     {
+//         console.log("it ia a odd number")
+//     }
+// }
+// a(3);
+
+
+// let name=(a)=>console.log("hello",a);
+// name("vishal");
+
+
+// let a=(b="guest")=>console.log(b);
+// a();
+// a("vishal");
+
+
+// let compare =(a,b,c)=>
+// {
+//     if(a===b&&b===c)
+//         {
+//             console.log(a,b,c,"all are equal")
+//         }
+//    else if(a>=b&&a>=c)
+//     {
+//          console.log(a,"is the greatest number")
+//     }
+//     else if (b>=a&&b>=c)
+//     {
+//         console.log(b,"is the greatest number ")
+//     }
+//     else{
+//         console.log(c,"is the greatest number")
+//     }
+// }
+// compare(23,34,45);
+// compare(23,23,23);
+// compare(45,34,45);
+// compare(23,23,45);
+// compare(23,34,34);
+
+
+// let a=(...args)=>
+//      {
+//       let  sum=0;
+//         for(let a of args) sum=sum+a;
+//         return console.log(sum);
+//         }
+//         a(12,23,3);
+
+// let multiply=(a,b)=>console.log(a*b);
+// multiply(9,0);
+// multiply(9,9);
+// multiply(9,1);
+
+
+
+// let a=[1,2,3,4];
+// b=a.map(x=>x*2)
+// console.log(b)
+
+
+// let a=[1,2,3,4];
+// b=a.map(x=>x*x)
+// console.log(b)
+
+// let a=[{name: "John"}, {name: "Jane"}];
+// let b=a.map(c=>c.name)
+// console.log(b);
