927a2cd659ae32189b1da316e7a4e154b27c3b0f
vk698 <vishalkumar2257r@gmail.com>
2026-04-14 16:05:34 +0530

    new commits

---
+++ firstProject/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./App.css"
+export default function App() {
+  return (
+    <div>
+    <div className='header'>
+      <a href="">Home</a>
+       <a href="">About us</a>
+        <a href="">Contact</a>
+         <a href="">Login</a>
+    </div>
+    <div className='footer'>
+hiii
+    </div>
+    </div>
+  )
+}
